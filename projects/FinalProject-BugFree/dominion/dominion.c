898091f6e71b11a089dbdafa44809c85b4b1cc15
Ashley Merriner <merrinea@oregonstate.edu>
2019-11-24 18:19:21 -0800

    WIP

--- projects/FinalProject-BugFree/dominion/dominion.c
+++ projects/FinalProject-BugFree/dominion/dominion.c
@@ -843,9 +843,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     case remodel:
         j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-        if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 2) < getCost(choice2) )
         {
-            return -1;
+            return -3;
         }
 
         gainCard(choice2, state, 0, currentPlayer);
@@ -863,7 +863,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
             }
         }
 
-
+		updateCoins(currentPlayer, state, 0);
         return 0;
 
     case smithy:
